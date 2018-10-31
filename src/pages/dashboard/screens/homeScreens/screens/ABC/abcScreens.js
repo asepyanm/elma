d6a72eca99aaa8a9c8ffc1de8231e9b9cb29259d
WoodsterId <kautsarajiw@gmail.com>
2018-10-31 11:31:14 +0700

    fix tab bar and style ABC screen

--- src/pages/dashboard/screens/homeScreens/screens/ABC/abcScreens.js
+++ src/pages/dashboard/screens/homeScreens/screens/ABC/abcScreens.js
@@ -5,14 +5,32 @@ import {
   Text,
   View
 } from 'react-native';
+import { Container, Header, Content, Tab, Tabs } from 'native-base';
+
+//screens
+import EbisScreens from './screens/EbisScreens';
+import DesScreens  from './screens/DesScreens';
+import DbsScreens  from './screens/DbsScreens';
+import DgsScreens  from './screens/DgsScreens';
 
 export default class ABCscreen extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          ABC Screen
-        </Text>
+        <Tabs tabBarUnderlineStyle={{backgroundColor: '#575F6A'}}>
+          <Tab heading="EBIS" tabStyle={styles.tabStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle} textStyle={styles.textStyle}>
+            <EbisScreens />
+          </Tab>
+          <Tab heading="DES" tabStyle={styles.tabStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle} textStyle={styles.textStyle}>
+            <DesScreens />
+          </Tab>
+          <Tab heading="DBS" tabStyle={styles.tabStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle} textStyle={styles.textStyle}>
+            <DbsScreens />
+          </Tab>
+          <Tab heading="DGS" tabStyle={styles.tabStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle} textStyle={styles.textStyle}>
+            <DgsScreens />
+          </Tab>
+        </Tabs>
       </View>
     );
   }
@@ -21,13 +39,17 @@ export default class ABCscreen extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabStyle:{
+    backgroundColor:'#575F6A'
   },
+  activeTabStyle:{
+    backgroundColor:'#95a5a6',
+  },
+  activeTextStyle:{
+    color:'#FFF'
+  },
+  textStyle:{
+    color:'#FFF'
+  }
 });
