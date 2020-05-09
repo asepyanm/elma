ce1cad9cb951448710770f7af03bbcda7eb1c7c7
Kautsar Tubagus Aji <kautsarajiw@gmail.com>
2020-05-09 15:56:58 +0700

    change api elma to telkom

--- src/config/api_service.js
+++ src/config/api_service.js
@@ -1,9 +1,9 @@
 module.exports = {
   // API:'https://warungkopidigital.000webhostapp.com/api'
-  API:'http://myadais.com/jenal/api',
-   //API:'http://192.168.8.7/elmaapi/api'
-  //  API:'http://192.168.8.101/api',
-   APITELKOM:'https://apifactory.telkom.co.id:8243',
-   API2:'http://myadais.com/elma/api',
+  API:'https://10.60.166.153/elma/api',
+  //API:'http://192.168.8.7/elmaapi/api'
+  //API:'http://192.168.8.101/api',
+  APITELKOM:'https://apifactory.telkom.co.id:8243',
+  API2:'https://10.60.166.153/elma/api',
 };
 
